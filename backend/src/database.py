ef8abb9329c6f5e4f92e31572da0c9fee826630e
Borjar3n <fborja1801@gmail.com>
2025-01-14 23:22:19 +0100

    feat: prueba4

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,9 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
-# Nota: Internamente seguimos usando el puerto 5432 porque es el puerto dentro del contenedor
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db:5432/inventory"
+# Usar variables de entorno
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+DB_HOST = os.getenv('DB_HOST', 'db')
+DB_PORT = os.getenv('DB_PORT', '5432')
+DB_NAME = os.getenv('DB_NAME', 'inventory')
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
